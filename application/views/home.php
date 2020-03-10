5ec342fa9df2d9e21b04b7e33c63d9993c8de825
msuva <rahadiyansuva@gmail.com>
2020-03-10 10:04:45 +0700

    front-end home

---
+++ application/views/home.php
@@ -0,0 +1,290 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row" style="background-color:#31A38E; padding-top: 4px;padding-bottom:4px;">
+        <div class="col-10" >
+          <img src="assets/twitter.svg" alt="" width="27px">
+          <img src="assets/facebook.svg" alt="" width="27px">
+          <img src="assets/instagram.svg" alt="" width="27px">
+          <img src="assets/google.svg" alt="" width="27px">
+          <span id="no-hp" style="color:white; padding-left:5px; font-weight: bold;">0852-8964-4888</span>
+
+        </div>
+
+        <div class="col-2">
+          <img src="assets/user.svg" alt="" width="23px">
+          <a href="#" style="color: white; font-weight:bold;">SIGN IN</a>
+
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <p style="font-size: 77px; text-align: center; padding-top: 14px;">Jon's</p>
+          <p style="text-align: center;">Boulangerie Patisserie</p>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-2">
+
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">HOME</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">FAQ</a></center>
+
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">JON'S</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">ABOUT US</a></center>
+        </div>
+
+        <div class="col-2">
+
+        </div>
+
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <center><img src="assets/kue.png" alt="" width="900px"></center>
+        </div>
+      </div>
+    </div>
+
+    <br>
+
+    <div class="container" style="">
+      <div class="row">
+        <div class="col">
+          <center><p>WELCOME TO JON'S Boulangerie Patisserie</p></center>
+          <hr style="">
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-4">
+          <center><img src="assets/welcome1.png" alt="" width="270px"></center>
+        </div>
+
+        <div class="col-4">
+          <center><img src="assets/welcome2.png" alt="" width="270px"></center>
+        </div>
+
+        <div class="col-4">
+          <center><img src="assets/welcome3.png" alt="" width="270px"></center>
+        </div>
+      </div>
+
+      <br>
+      <br>
+
+      <div class="row">
+        <div class="col">
+          <center><p>SHOP BY CATEGORIES</p></center>
+          <hr>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-4">
+          <center><img src="assets/kategori1.png" alt="" width="270px"></center>
+          <center><p>CUP CAKES</p></center>
+        </div>
+
+        <div class="col-4">
+          <center><img src="assets/kategori2.png" alt="" width="270px"></center>
+          <center><p>HOUSE CAKES</p></center>
+        </div>
+
+        <div class="col-4">
+          <center><img src="assets/kategori3.png" alt="" width="270px"></center>
+          <center><p>MACAROON</p></center>
+        </div>
+      </div>
+
+      <br>
+      <br>
+
+      <div class="row">
+        <div class="col-4">
+          <center><img src="assets/kategori4.png" alt="" width="270px"></center>
+          <center><p>BREADS</p></center>
+        </div>
+
+        <div class="col-4">
+          <center><img src="assets/kategori5.png" alt="" width="270px"></center>
+          <center><p>DOUGNAT</p></center>
+        </div>
+
+        <div class="col-4">
+          <center><img src="assets/kategori6.png" alt="" width="270px"></center>
+          <center><p>BREAKFAST PASTRIES</p></center>
+        </div>
+      </div>
+
+      <br>
+      <br>
+
+      <div class="row">
+        <div class="col">
+          <center><div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+              </ol>
+              <div class="carousel-inner" role="listbox">
+                <div class="carousel-item active">
+                  <img class="d-block img-fluid" src="assets/slide.png" alt="First slide" width="1030px">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block img-fluid" src="assets/slide.png" alt="Second slide" width="1030px">
+                </div>
+                <div class="carousel-item">
+                  <img class="d-block img-fluid" src="assets/slide.png" alt="Third slide" width="1030px">
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+              </a>
+          </div></center>
+        </div>
+      </div>
+
+      <br>
+      <br>
+
+      <div class="row">
+        <div class="col">
+          <center><p>FEATURED PRODUCT OF THE MONTH</p></center>
+          <hr>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-3">
+
+        </div>
+
+        <div class="col-3">
+          <center><img src="assets/potm.png" alt="" width="270px"></center>
+        </div>
+
+        <div class="col-3">
+          <p style="text-align:left;font-size:26px;">MACAROON</p>
+            <hr>
+            <p style="text-align: left;">Beautiful macaroons are one of the current ‘sweet’ crazes. Make a batch, put them in a pretty box tied up with ribbon, and give them to a friend as a dazzling gift.</p>
+              <a href="#" class="btn btn-primary" style="background-color:#31A38E">Rp. 50.000,-</a>
+        </div>
+
+        <div class="col-3">
+
+        </div>
+      </div>
+    </div>
+
+    <br>
+
+    <div class="container-fluid" style="">
+      <div class="row">
+        <div class="col-9">
+          <img src="assets/visit.png" alt="" width="1030px">
+        </div>
+
+        <div class="col-3" style="background-color:#E4E4E4;">
+          <p style="text-align:left; font-size:26px; padding-top:150px;">COME & VISIT</p>
+            <hr>
+            <p style="text-align: left;  font-weight: bold;">Jl. Jend. Sudirman No.55, Kota Bandar Lampung </p>
+            <p style="text-align: left;">Wednesday-Monday <br>6:30 AM - 9.30 PM </p>
+            <p style="text-align: left;">Tuesday <br>7.30 AM - 9.30 PM </p>
+              <a href="#" class="btn btn-primary" style="background-color:#31A38E">DIRECTION</a>
+        </div>
+      </div>
+    </div>
+
+    <br>
+    <br>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <center><p>RECENT TESTIMONIALS</p></center>
+          <hr>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-4">
+          <center><p>Great place to chill, get away, eat some good food. there was quick, helpful, and friendly service. Feels similar to a cafe in Chicago I loved.</p>
+          <p style="font-weight:bold">— Courtney Arnold</p></center>
+        </div>
+
+        <div class="col-4">
+          <center><p>I can say this place does it for me. Service is consistently above par. Bring your appetite because portions are large.</p>
+          <p style="font-weight:bold">— Emmett Carrillo</p></center>
+        </div>
+
+        <div class="col-4">
+          <center><p>My favorite place for THE BEST almond croissant and chocolate almond croissant in SF!!! Def worth the wait!</p>
+          <p style="font-weight:bold"> — Virginia Griffin</p></center>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row" style="background-color:#31A38E; padding-top: 20px;padding-bottom:20px;">
+        <div class="col-4">
+          <center><img src="assets/logo.png" alt="" width="155px"></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: white;">POLICY</a></center>
+          <center><a href="#" style="color: white;">Return</a></center>
+          <center><a href="#" style="color: white;">About Us</a></center>
+          <center><a href="#" style="color: white;">FAQ</a></center>
+          <center><a href="#" style="color: white;">Privacy Policy</a></center>
+          <center><a href="#" style="color: white;">Term & Conditions</a></center>
+        </div>
+
+        <div class="col-4">
+          <center><p style="color:white; font-weight:bold;">HI, YOU LET'S SIGN IN <br> AND <br> GET 20% OFF YOUR FIRST ONLINE</p>
+          <img src="assets/twitter.svg" alt="" width="27px">
+          <img src="assets/facebook.svg" alt="" width="27px">
+          <img src="assets/instagram.svg" alt="" width="27px">
+          <img src="assets/google.svg" alt="" width="27px"> </center>
+        </div>
+
+        <div class="col-2">
+          <center><p style="color:white; font-weight:bold;">VISIT US</p></center>
+          <p style="color:white;">Jl. Jend. Sudirman No.55, Kota Bandar Lampung</p>
+        </div>
+      </div>
+    </div>
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+  </body>
+</html>
