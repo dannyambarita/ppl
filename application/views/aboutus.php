d78e58862047e5d535d6595bade98550cf335343
msuva <rahadiyansuva@gmail.com>
2020-03-20 16:13:32 +0700

    front-end loc, about, catalog,kategori, desc

---
+++ application/views/aboutus.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row" style="background-color:#31A38E; padding-top: 4px;padding-bottom:4px;">
+        <div class="col-10" >
+          <img src="assets/twitter.svg" alt="" width="27px">
+          <img src="assets/facebook.svg" alt="" width="27px">
+          <img src="assets/instagram.svg" alt="" width="27px">
+          <img src="assets/google.svg" alt="" width="27px">
+          <span id="no-hp" style="color:white; padding-left:5px; font-weight: bold;">0852-8964-4888</span>
+
+        </div>
+
+        <div class="col-2">
+          <img src="assets/user.svg" alt="" width="23px">
+          <a href="#" style="color: white; font-weight:bold;">SIGN IN</a>
+
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <p style="font-size: 77px; text-align: center; padding-top: 14px;">Jon's</p>
+          <p style="text-align: center;">Boulangerie Patisserie</p>
+        </div>
+      </div>
+
+      <div class="row" style="background-color: #E5F1EF;padding-top: 4px;padding-bottom:4px;">
+        <div class="col-1">
+
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">HOME</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">LOCATION</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">CATALOG</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">FAQ</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">ABOUT US</a></center>
+        </div>
+
+        <div class="col-1">
+
+        </div>
+      </div>
+    </div>
+
+    <br>
+
+    <div class="container">
+      <div class="row">
+        <div class="col-5">
+          <center><img src="assets/aboutus.png" alt="" width="500px"></center>
+        </div>
+
+        <div class="col-1">
+
+        </div>
+
+        <div class="col-6">
+          <p style="font-size: 20px; text-align: center; padding-top: 20px;">ABOUT US</p><hr>
+          <p style="text-align: center; font-weight:bold;"> A Team That Feels a Whole Lot Like Family</p><p style="text-align: center;"> We, Mary and Michael Gassen, are the husband and wife founding team behind Jon’s Boulangerie Patisserie. Michael is our master baker and Mary is our strategic leader. We’re proud to have built a team of bakers and service professionals who delight in taking care of each other and serving our community.</p>
+          <br>
+          <p style="text-align: center; font-weight:bold;"> We Love Being A Bakery </p><p style="text-align:center;"> To us, a bakery is everything that’s right with a neighborhood. We love working dough in the early hours of the morning, the satisfyingly straight rows of baguette, and too-adorable-to-eat cupcakes. We love putting smiles on the faces of little ones, prepping for the big meeting with a great breakfast, and giving an excuse to lingering with an old friend. We love every single minute of being your San Francisco neighborhood bakery.</p>
+        </div>
+      </div>
+    </div>
+
+    <br>
+
+    <div class="container-fluid">
+      <div class="row" style="background-color:#31A38E; padding-top: 20px;padding-bottom:20px;">
+        <div class="col-4">
+          <center><img src="assets/logo.png" alt="" width="155px"></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: white;">POLICY</a></center>
+          <center><a href="#" style="color: white;">Return</a></center>
+          <center><a href="#" style="color: white;">About Us</a></center>
+          <center><a href="#" style="color: white;">FAQ</a></center>
+          <center><a href="#" style="color: white;">Privacy Policy</a></center>
+          <center><a href="#" style="color: white;">Term & Conditions</a></center>
+        </div>
+
+        <div class="col-4">
+          <center><p style="color:white; font-weight:bold;">HI, YOU LET'S SIGN IN <br> AND <br> GET 20% OFF YOUR FIRST ONLINE</p>
+          <img src="assets/twitter.svg" alt="" width="27px">
+          <img src="assets/facebook.svg" alt="" width="27px">
+          <img src="assets/instagram.svg" alt="" width="27px">
+          <img src="assets/google.svg" alt="" width="27px"> </center>
+        </div>
+
+        <div class="col-2">
+          <center><p style="color:white; font-weight:bold;">VISIT US</p></center>
+          <p style="color:white;">Jl. Jend. Sudirman No.55, Kota Bandar Lampung</p>
+        </div>
+      </div>
+    </div>
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+
+  </body>
+</html>
---
+++ application/views/cakesdeskripsi.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row" style="background-color:#31A38E; padding-top: 4px;padding-bottom:4px;">
+        <div class="col-10" >
+          <img src="assets/twitter.svg" alt="" width="27px">
+          <img src="assets/facebook.svg" alt="" width="27px">
+          <img src="assets/instagram.svg" alt="" width="27px">
+          <img src="assets/google.svg" alt="" width="27px">
+          <span id="no-hp" style="color:white; padding-left:5px; font-weight: bold;">0852-8964-4888</span>
+
+        </div>
+
+        <div class="col-2">
+          <img src="assets/user.svg" alt="" width="23px">
+          <a href="#" style="color: white; font-weight:bold;">SIGN IN</a>
+
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <p style="font-size: 77px; text-align: center; padding-top: 14px;">Jon's</p>
+          <p style="text-align: center;">Boulangerie Patisserie</p>
+        </div>
+      </div>
+
+      <div class="row" style="background-color: #E5F1EF;padding-top: 4px;padding-bottom:4px;">
+        <div class="col-1">
+
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">HOME</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">LOCATION</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">CATALOG</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">FAQ</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">ABOUT US</a></center>
+        </div>
+
+        <div class="col-1">
+
+        </div>
+      </div>
+    </div>
+
+    <br>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <p style="font-size: 20px; text-align: center; padding-top: 20px;">CAKES</p><hr>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-2">
+
+        </div>
+
+        <div class="col-4">
+          <center><img src="assets/cakes1.png" alt="" width="300px";></center>
+        </div>
+
+        <div class="col-4">
+          <p style="text-align:left;font-size:23px;">Choco Nut</p>
+            <hr>
+            <p style="text-align: left;">Almond flour gives a subtle, nutty richness to this cake, which has a deep chocolate flavor without being too sweet. Topped with espresso-laced ganache, it’s an excellent dinner party or Passover dessert.</p>
+            <p style="font-weight: bold;">Stok: 34</p>
+              <a href="#" class="btn btn-primary" style="background-color:#31A38E">Rp. 50.000,-</a>
+        </div>
+
+        <div class="col-2">
+
+        </div>
+      </div>
+
+      <br>
+
+      <div class="row">
+        <div class="col">
+          <p style="font-size: 20px; text-align: center; padding-top: 20px;">RELATED ITEMS</p><hr>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-4">
+          <center><img src="assets/catalog1.png" alt="" width="300px"></center>
+        </div>
+
+        <div class="col-4">
+          <center><img src="assets/cakes2.png" alt="" width="300px"></center>
+        </div>
+
+        <div class="col-4">
+          <center><img src="assets/catalog3.png" alt="" width="300px"></center>
+        </div>
+      </div>
+    </div>
+
+    <br>
+    <br>
+
+    <div class="container-fluid">
+      <div class="row" style="background-color:#31A38E; padding-top: 20px;padding-bottom:20px;">
+        <div class="col-4">
+          <center><img src="assets/logo.png" alt="" width="155px"></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: white;">POLICY</a></center>
+          <center><a href="#" style="color: white;">Return</a></center>
+          <center><a href="#" style="color: white;">About Us</a></center>
+          <center><a href="#" style="color: white;">FAQ</a></center>
+          <center><a href="#" style="color: white;">Privacy Policy</a></center>
+          <center><a href="#" style="color: white;">Term & Conditions</a></center>
+        </div>
+
+        <div class="col-4">
+          <center><p style="color:white; font-weight:bold;">HI, YOU LET'S SIGN IN <br> AND <br> GET 20% OFF YOUR FIRST ONLINE</p>
+          <img src="assets/twitter.svg" alt="" width="27px">
+          <img src="assets/facebook.svg" alt="" width="27px">
+          <img src="assets/instagram.svg" alt="" width="27px">
+          <img src="assets/google.svg" alt="" width="27px"> </center>
+        </div>
+
+        <div class="col-2">
+          <center><p style="color:white; font-weight:bold;">VISIT US</p></center>
+          <p style="color:white;">Jl. Jend. Sudirman No.55, Kota Bandar Lampung</p>
+        </div>
+      </div>
+    </div>
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+
+  </body>
+</html>
