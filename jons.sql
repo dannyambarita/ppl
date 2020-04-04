4304b6b02278409c7aba922b4120f4e65aa28075
dannyambarita <48155543+dannyambarita@users.noreply.github.com>
2020-04-05 01:56:17 +0700

    mengupdate kolom pengguna menjadi user

--- jons.sql
+++ jons.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 02, 2020 at 03:30 AM
+-- Generation Time: Apr 04, 2020 at 08:54 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.11
 
@@ -63,30 +63,6 @@ INSERT INTO `komentar` (`isi_komentar`, `id_komentar`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pengguna`
---
-
-CREATE TABLE `pengguna` (
-  `nama_pengguna` varchar(50) NOT NULL,
-  `umur_pengguna` varchar(200) NOT NULL,
-  `username_pengguna` varchar(50) NOT NULL,
-  `password_pengguna` varchar(50) NOT NULL,
-  `id_pengguna` int(255) NOT NULL,
-  `jenis_kelamin_pengguna` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `pengguna`
---
-
-INSERT INTO `pengguna` (`nama_pengguna`, `umur_pengguna`, `username_pengguna`, `password_pengguna`, `id_pengguna`, `jenis_kelamin_pengguna`, `email`) VALUES
-('Danny', '21', 'ekspulotion', 'asd', 3, 'laki', 'danny.ambarita130@gmail.com'),
-('hello world', '31', 'asdasd', 'asdasd', 4, 'perempuan', 'email@email.com');
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `rating`
 --
 
@@ -123,6 +99,54 @@ INSERT INTO `roti` (`nama_roti`, `harga_roti`, `jenis_roti`, `stok_roti`, `gamba
 ('roti coklat', 10000, 'enak', 12, '1574330094-picsay.jpg', 4),
 ('Christio Danny Gratia Ambarita', 10000, 'buns', 10, 'FC_Barcelona_(crest).svg.png', 5);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name_user` varchar(128) NOT NULL,
+  `age_user` int(11) NOT NULL,
+  `sex_user` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `image` varchar(128) NOT NULL,
+  `password` varchar(256) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `is_active` int(1) NOT NULL,
+  `date_created` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name_user`, `age_user`, `sex_user`, `email`, `image`, `password`, `role_id`, `is_active`, `date_created`) VALUES
+(1, 'Danny Ambarita', 21, 'man', 'danny.ambarita130@gmail.com', 'default.jpg', '$2y$10$dp5tBW0NnFe2O9b2sMrn3.UXSb/gckpyIYDFpbqtZvaOK2xJxGFmm', 2, 1, 1585962911),
+(3, 'Devina Sihaloho', 19, 'woman', 'devina.sihaloho71@gmail.com', 'default.jpg', '$2y$10$3CnstPJHKBpQCsiiJjlzTeYF9uXxSMSppbz8XTVAOgxT5q59t0O6a', 2, 1, 1585963021),
+(4, 'lionel messi', 33, 'man', 'lionel.messi@gmail.com', 'default.jpg', '$2y$10$kv2CEWSKh/wOpVKf5JGjHuA536A5O.cbnZyeZPXC0RTeZXLroNz/.', 2, 1, 1585963352),
+(18, 'Christio Danny Gratia Ambarita', 21, 'man', 'ambarita.danny@gmail.com', 'default.jpg', '$2y$10$SZEDgp9NYovJmPWui5CifevJKZBYpT1rIS5Pde.wDNGLY6rrTLygW', 2, 1, 1586022644);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL,
+  `role` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `role`) VALUES
+(1, 'admin'),
+(2, 'user');
+
 --
 -- Indexes for dumped tables
 --
@@ -140,12 +164,6 @@ ALTER TABLE `komentar`
   ADD PRIMARY KEY (`id_komentar`);
 
 --
--- Indexes for table `pengguna`
---
-ALTER TABLE `pengguna`
-  ADD PRIMARY KEY (`id_pengguna`);
-
---
 -- Indexes for table `rating`
 --
 ALTER TABLE `rating`
@@ -156,6 +174,18 @@ ALTER TABLE `rating`
 --
 ALTER TABLE `roti`
   ADD PRIMARY KEY (`id_roti`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -174,12 +204,6 @@ ALTER TABLE `komentar`
   MODIFY `id_komentar` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `pengguna`
---
-ALTER TABLE `pengguna`
-  MODIFY `id_pengguna` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
 -- AUTO_INCREMENT for table `rating`
 --
 ALTER TABLE `rating`
@@ -190,6 +214,18 @@ ALTER TABLE `rating`
 --
 ALTER TABLE `roti`
   MODIFY `id_roti` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `user_role`
+--
+ALTER TABLE `user_role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
