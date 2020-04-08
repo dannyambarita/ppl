deb1f673c92d1925c086a32590a2e722e37e1e97
msuva <rahadiyansuva@gmail.com>
2020-04-08 21:36:08 +0700

    dashboard menubar-front end

---
+++ application/views/menubar.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+<style media="screen">
+ #overview{
+   background-color: #4f4f4f;
+ }
+  #overview:hover{
+    background-color: #31A38E;
+  }
+
+#product{
+  background-color: #4f4f4f;
+}
+ #product:hover{
+   background-color: #31A38E;
+ }
+
+#kategori{
+ background-color: #4f4f4f;
+}
+#kategori:hover{
+  background-color: #31A38E;
+}
+
+#testimoni{
+  background-color: #4f4f4f;
+}
+ #testimoni:hover{
+   background-color: #31A38E;
+ }
+
+#menubar{
+ background-color: #4f4f4f;
+}
+#menubar:hover{
+  background-color: #31A38E;
+}
+
+#user{
+  background-color: #4f4f4f;
+}
+ #user:hover{
+   background-color: #31A38E;
+ }
+
+.tom-nav{
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+</style>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row" style="background-color:#31A38E; padding-top: 4px;padding-bottom:4px;">
+        <div class="col-10" >
+          <img src="assets/twitter.svg" alt="" width="27px">
+          <img src="assets/facebook.svg" alt="" width="27px">
+          <img src="assets/instagram.svg" alt="" width="27px">
+          <img src="assets/google.svg" alt="" width="27px">
+          <span id="no-hp" style="color:white; padding-left:5px; font-weight: bold;">0852-8964-4888</span>
+
+        </div>
+
+        <div class="col-2">
+          <img src="assets/user.svg" alt="" width="23px">
+          <a href="#" style="color: white; font-weight:bold;">SIGN IN</a>
+        </div>
+      </div>
+
+      <br>
+
+      <div class="row" style="padding-top: 4px;padding-bottom:4px;">
+        <div class="col-3" style="background-color: #4f4f4f;">
+          <div class="row" style="background-color: #31A38E;">
+            <div class="col" style="color: white; font-weight:bold;">
+              <center>Jon's Admin</center>
+            </div>
+          </div>
+
+          <div class="row" style="background-color: #4f4f4f;">
+            <div class="col">
+              <br>
+              <center><img src="assets/ava.jpg" alt="" width="85px">
+              <p style="color: white;">Danny Ambyar</p></center>
+            </div>
+          </div>
+
+          <div class="row tom-nav" style="" id="overview">
+            <div class="col">
+              <center><a href="#" style="color: white;">Over View</a></center>
+            </div>
+          </div>
+
+          <div class="row tom-nav" style="" id="product">
+            <div class="col">
+              <center><a href="#" style="color: white;">Product</center>
+            </div>
+          </div>
+
+          <div class="row tom-nav" style="" id="kategori">
+            <div class="col">
+              <center><a href="#" style="color: white;">Categories</a></center>
+            </div>
+          </div>
+
+          <div class="row tom-nav" style="" id="testimoni">
+            <div class="col">
+              <center><a href="#" style="color: white;">Testimoni</a></center>
+            </div>
+          </div>
+
+          <div class="row tom-nav" style="" id="menubar">
+            <div class="col">
+              <center><a href="#" style="color: white;">Menu Bar</a></center>
+            </div>
+          </div>
+
+          <div class="row tom-nav" style="" id="user">
+            <div class="col">
+              <center><a href="#" style="color: white;">User</a></center>
+            </div>
+          </div>
+
+          <div class="row" style="background-color: #4f4f4f;">
+
+          </div>
+        </div>
+
+        <div class="col-9">
+          <div class="row" style="background-color: #4f4f4f; color: #4f4f4f">
+            .
+          </div>
+
+          <br>
+
+          <div class="row">
+            <div class="col">
+              <!-- ABOUT US -->
+              <div class="container" style="background-color: #f0eee4; margin-left: 10px; margin-right: 10px;">
+                <div class="row" style="background-color: #d6d6d6;">
+                  <div class="col">
+                    <center><a href="" style=" color: #31A38E; font-weight: bold;">About Us</a></center>
+                  </div>
+                </div>
+
+                <br>
+
+                <div class="row">
+                  <div class="col">
+                    <h6>Description</h6>
+                    <textarea name="deskripsi" rows="2" cols="75"></textarea><br><br>
+                    <h6>Photo</h6>
+                    <button type="button" class="btn btn-primary" style=" background-color: #31A38E; color: white;" >Choose File</button>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-10">
+
+                  </div>
+
+                  <div class="col-2">
+                    <button type="button" class="btn btn-primary" style=" background-color: #31A38E; color: white;" >Save</button>
+                  </div>
+                </div>
+                <br>
+              </div>
+              <br>
+
+              <!-- LOCATION -->
+
+              <div class="container" style="background-color: #f0eee4; margin-left: 10px; margin-right: 10px;">
+                <div class="row" style="background-color: #d6d6d6;">
+                  <div class="col">
+                    <center><a href="" style=" color: #31A38E; font-weight: bold;0">Location</a></center>
+                  </div>
+                </div>
+
+                <br>
+
+                <div class="row">
+                  <div class="col">
+                    <h6>Location</h6>
+                    <textarea name="location" rows="1" cols="75"></textarea><br><br>
+                    <h6>On the day</h6>
+                    <textarea name="ontheday" rows="1" cols="75"></textarea><br><br>
+                    <h6>Open</h6>
+                    <textarea name="open" rows="1" cols="20"></textarea>
+                    <h6>Close</h6>
+                    <textarea name="close" rows="1" cols="20"></textarea><br><br>
+                    <h6>Photo</h6>
+                    <button type="button" class="btn btn-primary" style=" background-color: #31A38E; color: white;" >Choose File</button>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-10">
+
+                  </div>
+
+                  <div class="col-2">
+                    <button type="button" class="btn btn-primary" style=" background-color: #31A38E; color: white;" >Save</button>
+                  </div>
+                </div>
+                <br>
+              </div>
+              <br>
+
+              <!-- FAQ -->
+              <div class="container" style="background-color: #f0eee4; margin-left: 10px; margin-right: 10px;">
+                <div class="row" style="background-color: #d6d6d6;">
+                  <div class="col">
+                    <center><a href="" style=" color: #31A38E; font-weight: bold;">FAQ</a></center>
+                  </div>
+                </div>
+
+                <br>
+
+                <div class="row">
+                  <div class="col">
+                    <h6>FAQ</h6>
+                    <textarea name="faq" rows="2" cols="75"></textarea><br><br>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="col-10">
+
+                  </div>
+
+                  <div class="col-2">
+                    <button type="button" class="btn btn-primary" style=" background-color: #31A38E; color: white;" >Save</button>
+                  </div>
+                </div>
+                <br>
+              </div>
+              <br>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <center><footer style="background-color: #d6d6d6;">Copyriht</footer></center>
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+
+  </body>
+</html>
---
+++ application/views/profil.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row" style="background-color:#31A38E; padding-top: 4px;padding-bottom:4px;">
+        <div class="col-10" >
+          <img src="assets/twitter.svg" alt="" width="27px">
+          <img src="assets/facebook.svg" alt="" width="27px">
+          <img src="assets/instagram.svg" alt="" width="27px">
+          <img src="assets/google.svg" alt="" width="27px">
+          <span id="no-hp" style="color:white; padding-left:5px; font-weight: bold;">0852-8964-4888</span>
+
+        </div>
+
+        <div class="col-2">
+          <img src="assets/user.svg" alt="" width="23px">
+          <a href="#" style="color: white; font-weight:bold;">SIGN IN</a>
+
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col">
+          <p style="font-size: 77px; text-align: center; padding-top: 14px;">Jon's</p>
+          <p style="text-align: center;">Boulangerie Patisserie</p>
+        </div>
+      </div>
+
+      <div class="row" style="background-color: #E5F1EF;padding-top: 4px;padding-bottom:4px;">
+        <div class="col-1">
+
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">HOME</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">LOCATION</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">CATALOG</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">FAQ</a></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: black;">ABOUT US</a></center>
+        </div>
+
+        <div class="col-1">
+
+        </div>
+      </div>
+    </div>
+
+    <br>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col">
+          <p style="font-size: 20px; text-align: center; padding-top: 20px;">ACCOUNT DETAILS</p><hr>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-1">
+
+        </div>
+
+        <div class="col-4">
+          <p style="font-size: 16px; text-align: left; padding-top: 20px;">Edo df</p><hr>
+        </div>
+
+        <div class="col-1">
+
+        </div>
+
+        <div class="col-5">
+          <p style="font-size: 16px; text-align: left; padding-top: 20px;">Comment History</p><hr>
+        </div>
+
+        <div class="col-1">
+
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-1">
+
+        </div>
+
+        <div class="col-4">
+          <p style="font-size: 16 px; text-align: left;">edofirmansyah23@gmail.com</p>
+        </div>
+
+        <div class="col-1">
+
+        </div>
+
+        <div class="col-5">
+          <p style="font-size: 16 px; text-align: left;">Great place to chill, get away, eat some good food</p>
+        </div>
+
+        <div class="col-1">
+          <img src="assets/panah.png" alt="" width="27px">
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-1">
+
+        </div>
+
+        <div class="col-4">
+          <hr>
+          <p>How do you feel about our product?</p>
+          <textarea name="comment" rows="2" cols="25"></textarea><br>
+          <button type="button" class="btn btn-primary" style="background: #31A38E;">SEND</button>
+        </div>
+      </div>
+    </div>
+
+    <br>
+
+    <div class="container-fluid">
+      <div class="row" style="background-color:#31A38E; padding-top: 20px;padding-bottom:20px;">
+        <div class="col-4">
+          <center><img src="assets/logo.png" alt="" width="155px"></center>
+        </div>
+
+        <div class="col-2">
+          <center><a href="#" style="color: white;">POLICY</a></center>
+          <center><a href="#" style="color: white;">Return</a></center>
+          <center><a href="#" style="color: white;">About Us</a></center>
+          <center><a href="#" style="color: white;">FAQ</a></center>
+          <center><a href="#" style="color: white;">Privacy Policy</a></center>
+          <center><a href="#" style="color: white;">Term & Conditions</a></center>
+        </div>
+
+        <div class="col-4">
+          <center><p style="color:white; font-weight:bold;">HI, YOU LET'S SIGN IN <br> AND <br> GET 20% OFF YOUR FIRST ONLINE</p>
+          <img src="assets/twitter.svg" alt="" width="27px">
+          <img src="assets/facebook.svg" alt="" width="27px">
+          <img src="assets/instagram.svg" alt="" width="27px">
+          <img src="assets/google.svg" alt="" width="27px"> </center>
+        </div>
+
+        <div class="col-2">
+          <center><p style="color:white; font-weight:bold;">VISIT US</p></center>
+          <p style="color:white;">Jl. Jend. Sudirman No.55, Kota Bandar Lampung</p>
+        </div>
+      </div>
+    </div>
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+
+  </body>
+</html>
