6fb39d1764cefd44e9a16627c7a286c27b7a16e8
msuva <rahadiyansuva@gmail.com>
2020-04-09 09:37:49 +0700

    product/add

---
+++ application/views/product.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+<style media="screen">
+ #overview{
+   background-color: #4f4f4f;
+ }
+  #overview:hover{
+    background-color: #31A38E;
+  }
+
+#product{
+  background-color: #4f4f4f;
+}
+ #product:hover{
+   background-color: #31A38E;
+ }
+
+#kategori{
+ background-color: #4f4f4f;
+}
+#kategori:hover{
+  background-color: #31A38E;
+}
+
+#testimoni{
+  background-color: #4f4f4f;
+}
+ #testimoni:hover{
+   background-color: #31A38E;
+ }
+
+#menubar{
+ background-color: #4f4f4f;
+}
+#menubar:hover{
+  background-color: #31A38E;
+}
+
+#user{
+  background-color: #4f4f4f;
+}
+ #user:hover{
+   background-color: #31A38E;
+ }
+
+.tom-nav{
+  padding-top: 10px;
+  padding-bottom: 10px;
+}
+</style>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+  </head>
+  <body>
+    <div class="container-fluid">
+      <div class="row" style="background-color:#31A38E; padding-top: 4px;padding-bottom:4px;">
+        <div class="col-10" >
+          <img src="assets/twitter.svg" alt="" width="27px">
+          <img src="assets/facebook.svg" alt="" width="27px">
+          <img src="assets/instagram.svg" alt="" width="27px">
+          <img src="assets/google.svg" alt="" width="27px">
+          <span id="no-hp" style="color:white; padding-left:5px; font-weight: bold;">0852-8964-4888</span>
+
+        </div>
+
+        <div class="col-2">
+          <img src="assets/user.svg" alt="" width="23px">
+          <a href="#" style="color: white; font-weight:bold;">SIGN IN</a>
+        </div>
+      </div>
+
+      <br>
+
+      <div class="row" style="padding-top: 4px;padding-bottom:4px;">
+        <div class="col-3" style="background-color: #4f4f4f;">
+          <div class="row" style="background-color: #31A38E;">
+            <div class="col" style="color: white; font-weight:bold;">
+              <center>Jon's Admin</center>
+            </div>
+          </div>
+
+          <div class="row" style="background-color: #4f4f4f;">
+            <div class="col">
+              <br>
+              <center><img src="assets/ava.jpg" alt="" width="85px">
+              <p style="color: white;">Danny Ambyar</p></center>
+            </div>
+          </div>
+
+          <div class="row tom-nav" style="" id="overview">
+            <div class="col">
+              <center><a href="#" style="color: white;">Over View</a></center>
+            </div>
+          </div>
+
+          <div class="row tom-nav" style="" id="product">
+            <div class="col">
+              <center><a href="#" style="color: white;">Product</center>
+            </div>
+          </div>
+
+          <div class="row tom-nav" style="" id="kategori">
+            <div class="col">
+              <center><a href="#" style="color: white;">Categories</a></center>
+            </div>
+          </div>
+
+          <div class="row tom-nav" style="" id="testimoni">
+            <div class="col">
+              <center><a href="#" style="color: white;">Testimoni</a></center>
+            </div>
+          </div>
+
+          <div class="row tom-nav" style="" id="menubar">
+            <div class="col">
+              <center><a href="#" style="color: white;">Menu Bar</a></center>
+            </div>
+          </div>
+
+          <div class="row tom-nav" style="" id="user">
+            <div class="col">
+              <center><a href="#" style="color: white;">User</a></center>
+            </div>
+          </div>
+
+          <div class="row" style="background-color: #4f4f4f;">
+
+          </div>
+        </div>
+
+        <div class="col-9">
+          <div class="row" style="background-color: #4f4f4f; color: #4f4f4f">
+            .
+          </div>
+
+          <br>
+
+          <div class="row">
+            <div class="col">
+              <!-- ABOUT US -->
+              <div class="container" style="background-color: #f0eee4; margin-left: 10px; margin-right: 10px;">
+                <div class="row" style="background-color: #d6d6d6;">
+                  <div class="col">
+                    <a href="" style=" color: #31A38E; font-weight: bold;">+Add product</a>
+                  </div>
+                </div>
+
+                <br>
+
+                <div class="row">
+                  <div class="col-10">
+
+                  </div>
+
+                  <div class="col-2">
+                    <button type="button" class="btn btn-primary" style=" background-color: #31A38E; color: white;" >Save</button>
+                  </div>
+                </div>
+                <br>
+              </div>
+              <br>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <center><footer style="background-color: #d6d6d6;">Copyriht</footer></center>
+
+    <script src="js/jquery-3.4.1.min.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+
+  </body>
+</html>
