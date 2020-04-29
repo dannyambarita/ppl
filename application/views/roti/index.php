7b14af81e2103fe535002c3a50d1245f87192aeb
dannyambarita <48155543+dannyambarita@users.noreply.github.com>
2020-04-29 22:29:13 +0700

    update css catalog

--- application/views/roti/index.php
+++ application/views/roti/index.php
@@ -36,10 +36,35 @@
           we always serve the best</p><br>
       </div>
     </div>
-    <div class="flex-container">
-      <?php foreach ($roti as $rt) : ?>
-        <td>
-          <center>
+
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+      <style>
+        .flex-container {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+        }
+
+        .flex-container>div {
+          background-color: #f1f1f1;
+          width: 100px;
+          margin: 10px;
+          text-align: center;
+          line-height: 75px;
+          font-size: 30px;
+        }
+      </style>
+    </head>
+
+    <body>
+
+      <div class="flex-container">
+        <?php foreach ($roti as $rt) : ?>
+          <td>
+
             <div class="card" style="width: 21rem;">
               <div class="col-auto" style="background-color: black"><br>
                 <center><img src="<?= base_url('assets/img/roti/') . $rt['gambar_roti']; ?>" width="300px"></center>
@@ -49,72 +74,10 @@
                     -</p><a href="<?= base_url('Deskripsi/detail/') ?><?= $rt['id_roti'] ?>" class="btn" style="background-color: #31A38E; color: white;">Detail</a>
                 </center><br>
               </div>
-          </center>
-    </div>
-    </td><br>
-  </div><?php endforeach; ?>
-</div>
-<div class="flex-container">
-  <div>1</div>
-  <div>2</div>
-  <div>3</div>
-  <div>4</div>
-  <div>5</div>
-  <div>6</div>
-  <div>7</div>
-  <div>8</div>
-  <div>9</div>
-  <div>10</div>
-  <div>11</div>
-  <div>12</div>
-</div>
+            </div>
+          </td><br><?php endforeach; ?>
+      </div>
 
-</body>
+    </body>
 
-<!DOCTYPE html>
-<html>
-
-<head>
-  <style>
-    .flex-container {
-      display: flex;
-      flex-wrap: wrap;
-      background-color: DodgerBlue;
-    }
-
-    .flex-container>div {
-      background-color: #f1f1f1;
-      width: 100px;
-      margin: 10px;
-      text-align: center;
-      line-height: 75px;
-      font-size: 30px;
-    }
-  </style>
-</head>
-
-<body>
-  <h1>The flex-wrap Property</h1>
-
-  <p>The "flex-wrap: wrap;" specifies that the flex items will wrap if necessary:</p>
-
-  <div class="flex-container">
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>4</div>
-    <div>5</div>
-    <div>6</div>
-    <div>7</div>
-    <div>8</div>
-    <div>9</div>
-    <div>10</div>
-    <div>11</div>
-    <div>12</div>
-  </div>
-
-  <p>Try resizing the browser window.</p>
-
-</body>
-
-</html>
+    </html>
