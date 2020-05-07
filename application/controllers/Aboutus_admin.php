46941f47dfefabde1403b1345747f4282fbcc740
msuva <rahadiyansuva@gmail.com>
2020-05-07 19:17:13 +0700

    fix aboutus

--- application/controllers/Aboutus_admin.php
+++ application/controllers/Aboutus_admin.php
@@ -15,16 +15,17 @@ class Aboutus_admin extends CI_Controller
     {
         $data['title'] = 'Admin Dashboard';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
-        $data['about'] = $this->db->get_where('about', array('id' => 1))->row_array();
+        $data['about'] = $this->about_us_model->about_us();
 
         $this->form_validation->set_rules('description', 'Description', 'required|trim');
+
         if ($this->form_validation->run() == false) {
             $this->load->view('templates/header2', $data);
             $this->load->view('aboutus_admin/index', $data);
         } else {
             $description = $this->input->post('description');
             $this->db->set('deskripsi', $description);
-            redirect('aboutus_admin');
+            redirect('aboutus');
         }
     }
 
